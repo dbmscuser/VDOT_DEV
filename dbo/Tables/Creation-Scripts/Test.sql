e7915938e7c6dfddc36a205487aa4f65ab7e8030
dbmscuser <dbmscuser>
2024-07-02 13:42:27 +0000

    Commit Test Table

---
+++ dbo/Tables/Creation-Scripts/Test.sql
@@ -0,0 +1,16 @@
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Test](
+	[emp_id] [int] NOT NULL,
+	[emp_name] [varchar](100) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
+	[phone_number] [dbo].[PhoneNumberType] NULL,
+	[dept_id] [int] NULL,
+	[position_id] [int] NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[emp_id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF)
+)
+GO
